4ed4e0e616862479530c8364a1b3670bbfc8ca10
brittikbasu <brittikbasu@gmail.com>
2017-03-07 10:33:18 +0530

    consistant brand colors &  design improvements

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Content } from 'ionic-angular';
+
+import { BravecamPage } from '../bravecam/bravecam';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  queries: {
+    content: new ViewChild('content')
+  }
 })
 export class HomePage {
+    @ViewChild(Content) content: Content;
+    bravecamPage = BravecamPage;
 
+cardOpened : boolean;
+openCard : {
+  one:boolean,
+  two:boolean,
+  three:boolean,
+  four:boolean
+};
+allCardsClosed : boolean;
   constructor(public navCtrl: NavController) {
+    this.allCardsClosed = true;
+    this.cardOpened = false;
+    this.openCard = {
+  one:false,
+  two:false,
+  three:false,
+  four:false
+};
+
 
   }
-
 }
+
