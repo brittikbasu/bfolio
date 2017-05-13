f770fa2b4f49cfaebd5fc86ca36517c3f1b6e85c
brittikbasu <brittikbasu@gmail.com>
2017-05-13 21:09:44 +0530

    implement slides, move tabs to the bottom

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild} from '@angular/core';
 
-import { NavController, Content, AlertController } from 'ionic-angular';
+import { NavController, Content, AlertController, Slides } from 'ionic-angular';
 
 import { BravecamPage } from '../bravecam/bravecam';
 
@@ -10,7 +10,6 @@ import { PolysparesPage } from '../polyspares/polyspares';
 
 import { PolizeigewaltPage } from '../polizeigewalt/polizeigewalt';
 
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
@@ -19,25 +18,28 @@ import { PolizeigewaltPage } from '../polizeigewalt/polizeigewalt';
   }
 })
 export class HomePage {
-    @ViewChild(Content) content: Content;
+  //  @ViewChild(Content) content: Content;
+  @ViewChild(Slides) slides: Slides;
     bravecamPage = BravecamPage;
     chaldiyePage = ChaldiyePage;
     polysparesPage = PolysparesPage;
     polizeigewaltPage = PolizeigewaltPage;
-    myAlert: boolean;
+   // myAlert: boolean;
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-        this.myAlert = true;
   }
 
+onScroll(){
+  //alert("scrolled");
+}
   ionViewDidLoad() {
-
+/*
 let alert = this.alertCtrl.create({
       title: 'Hi There Awesome Person!',
       subTitle: 'My website is a work in progress and will be ready by the end of this month (March, 2017). Dont let this stop you from checking it out now :)',
       buttons: ['OK COOL!']
     });
     alert.present();
-
+*/
     
   }
 
